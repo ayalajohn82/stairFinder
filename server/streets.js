8685b9c0f96d4649701d1f0c738fcf8441d621f8
ayala <deus@ubuntu.ubuntu-domain>
2018-02-19 11:58:36 -0800

    xml to json conversion

--- server/streets.js
+++ server/streets.js
@@ -1,11 +1,14 @@
 const axios = require('axios');
+const convert = require('xml-js');
+const options = {ignoreComment: true, alwaysChildren: true};
 
 var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ];
 
 const streetLocations = () => {
-  axios.get('https://api.openstreetmap.org/api/0.6/map?bbox=-122.75,36.8,-121.75,37.8')
+  axios.get('https://api.openstreetmap.org/api/0.6/map?bbox=-122.29,37.70,-122.22,37.71')
     .then((response) => {
-      console.log(response); 
+      var result = convert.xml2js(response.data, options).elements[0].elements; // or convert.xml2json(xml, options)
+      console.log(result); 
     })
     .catch((error) => {
       console.log(error);
